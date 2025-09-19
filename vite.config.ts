2512058952860e4eccb944aef944028b3f812d70
Mappledude <sablani.zane@gmail.com>
2025-09-19 01:57:23 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  server: { port: 5173 },
+  plugins: [react()],
+  server: { host: true, port: 5173, strictPort: true },
   preview: { port: 4173 }
 });
