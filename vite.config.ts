90344422b76f2b7be5cd336e08699befbddac414
Mappledude <sablani.zane@gmail.com>
2025-09-19 17:00:09 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,14 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: [
+      "firebase/app",
+      "firebase/auth",
+      "firebase/firestore",
+    ],
+  },
   server: { port: 5173 },
-  preview: { port: 4173 }
+  preview: { port: 4173 },
 });
 TS
