d35422f0891548b7e36e9300d74142a7e6365985
Mappledude <sablani.zane@gmail.com>
2025-09-21 13:21:55 -0400

    Import debug logging helpers in arena page

--- src/pages/ArenaPage.tsx
+++ src/pages/ArenaPage.tsx
@@ -14,6 +14,7 @@ import {
   type LeaderboardEntry,
 } from "../firebase";
 } from "../firebase";
+import { debugLog, debugWarn } from "../net/debug";
 import {
   ensureArenaState,
   watchArenaState,
